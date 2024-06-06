dea7447796b0850915a3414ca0845ab7c0ef2c25
GracyDinma <okohagchidinma@gmail.com>
2024-06-05 19:20:28 -0700

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 
